7ed76318c1bcb1b57d7fcb8eb485b60e3473b3a4
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-07-18 15:51:47 -0500

    Deleted friends-routes and added them to user-routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
-const friendsRoutes = require('./friends-routes');
 
 router.use('/users', userRoutes);
-router.use('/users', friendsRoutes);
 
 module.exports = router;
