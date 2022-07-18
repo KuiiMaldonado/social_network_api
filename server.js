5f3bce2952a2260ea587e40321e4501a79e07d1f
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-07-18 16:49:24 -0500

    Connecting and starting the server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./controllers');
+const db = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,3 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(routes);
 
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
