561766855d6d4ff7bedff91353be55d48ad2ae82
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-07-21 15:41:37 -0500

    Populating friends list when searching for user by id

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -23,6 +23,7 @@ router.get('/', (req, res) => {
 router.get('/:userId', async (req, res) => {
     try {
         const user = await User.findById(req.params.userId).exec();
+        await user.populate('friends');
         if (user)
             res.status(200).json(user);
         else
